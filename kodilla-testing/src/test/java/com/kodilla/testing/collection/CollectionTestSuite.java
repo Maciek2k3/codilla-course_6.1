c83386637f19a1d92eb34e9c7b3837cb79f9b0ae
Maciek2k3 <maciej.palczewski88@gmail.com>
2020-10-07 19:00:53 +0200

    6.3 Test List with even numbers
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.collection;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionTestSuite {
+
+    @BeforeEach
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Test Suite: begin");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Test Suite: end");
+    }
+    @DisplayName("when create empty List, " +
+            "then list should return true")
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        OddNumbersExterminator emptyExaminator=new OddNumbersExterminator();
+        List<Integer> emptyList= Arrays.asList();
+        //When
+        List<Integer> exam=emptyExaminator.exterminate(emptyList);
+        //Then
+        assertThat(exam).isEmpty();
+    }
+    @DisplayName("when create list with odd and even number, " +
+            "then list should return only even numbers, plus even List size with only even numbers")
+    @Test
+    public void  OddNumbersExterminatorNormalList(){
+        //Given
+        OddNumbersExterminator oddNumbersExterminator=new OddNumbersExterminator();
+        List<Integer> evenList=Arrays.asList(4,5,7,9);
+        //When
+        List<Integer> examEven=oddNumbersExterminator.exterminate(evenList);
+        //Then
+        assertThat(examEven).containsExactly(4);
+        assertThat(examEven.size()).isEqualTo(1);
+    }
+}
