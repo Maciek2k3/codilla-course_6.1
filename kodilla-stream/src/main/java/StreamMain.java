c59194af9a0f805f96da3f2b9463a6e185638b98
Maciek2k3 <maciej.palczewski88@gmail.com>
2020-10-13 19:20:50 +0200

    7.1 Lambda, stream, beautifull text
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

---
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -0,0 +1,32 @@
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        //System.out.println("Welcome to module 7 - Stream");
+        //SaySomething saySomething=new SaySomething();
+        //saySomething.say();
+        //Processor processor= new Processor();
+        //ExecuteSaySomething executeSaySomething=new ExecuteSaySomething();
+        //processor.execute(executeSaySomething);
+        //ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        //System.out.println("Calculating expressions with method references");
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //System.out.println("--------------------------------");
+        PoemBeautifier poemBeautifier=new PoemBeautifier();
+        poemBeautifier.beautify("Text1",text -> text.toUpperCase());
+        poemBeautifier.beautify("Text2",text -> "ABC"+text+"ABC");
+        poemBeautifier.beautify("Text3    ",text -> text.trim());
+        poemBeautifier.beautify("aaaText4bbb",text -> text.substring(3,8));
+        poemBeautifier.beautify("TEXT5", text ->text.toLowerCase());
+    }
+}
+
