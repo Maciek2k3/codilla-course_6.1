1be971127db9ce43eff798325dc0b4214c677646
Maciek2k3 <maciej.palczewski88@gmail.com>
2020-10-15 21:36:54 +0200

    7.5 IntStreamRange
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,8 +4,8 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    static double getAverage(int[] numbers){
-        IntStream.range(0,numbers.length).map(n->numbers[n]).forEach(System.out::println);
-        return IntStream.range(0,numbers.length).map(n->numbers[n]).average().getAsDouble();
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length).map(n -> numbers[n]).forEach(System.out::println);
+        return IntStream.range(0, numbers.length).map(n -> numbers[n]).average().getAsDouble();
     }
 }
