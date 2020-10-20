7d38bcf17c91929be6d1d988049cb00bfd76ab71
Maciek2k3 <maciej.palczewski88@gmail.com>
2020-10-20 21:37:56 +0200

    7.7 Portfolio Project with Extras
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

--- kodilla-rps/src/main/java/com/kodilla/rps/ComputerRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/ComputerRunner.java
@@ -11,13 +11,17 @@ public class ComputerRunner {
         List<Integer> rocket = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
             rocket.add(2);
+            rocket.add(5);
         }
         for (int a = 0; a < 25; a++) {
             rocket.add(3);
+            rocket.add(4);
         }
         for (int b = 0; b < 25; b++) {
             rocket.add(1);
+
         }
+
         int userRocket = random.nextInt(100);
         return rocket.get(userRocket);
     }
@@ -28,10 +32,12 @@ public class ComputerRunner {
             paper.add(2);
         }
         for (int a = 0; a < 25; a++) {
-            paper.add(3);
+            paper.add(1);
+            paper.add(5);
         }
         for (int b = 0; b < 50; b++) {
-            paper.add(1);
+            paper.add(3);
+            paper.add(4);
         }
         int userPaper = random.nextInt(100);
         return paper.get(userPaper);
@@ -43,15 +49,50 @@ public class ComputerRunner {
             scissors.add(2);
         }
         for (int a = 0; a < 50; a++) {
-            scissors.add(3);
+            scissors.add(1);
+            scissors.add(5);
         }
         for (int b = 0; b < 25; b++) {
-            scissors.add(1);
+            scissors.add(3);
+            scissors.add(4);
         }
         int userScissors = random.nextInt(100);
         return scissors.get(userScissors);
+    }
+
+    public int userBlizzard() {
+        List<Integer> blizzard = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            blizzard.add(2);
+        }
+        for (int a = 0; a < 50; a++) {
+            blizzard.add(3);
+            blizzard.add(1);
+        }
+        for (int b = 0; b < 25; b++) {
+            blizzard.add(4);
+            blizzard.add(5);
+        }
+        int userBlizzard = random.nextInt(100);
+        return blizzard.get(userBlizzard);
 
     }
 
+    public int userSpock() {
+        List<Integer> spock = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            spock.add(1);
+        }
+        for (int a = 0; a < 50; a++) {
+            spock.add(4);
+            spock.add(2);
+        }
+        for (int b = 0; b < 25; b++) {
+            spock.add(3);
+            spock.add(5);
+        }
+        int userSpock = random.nextInt(100);
+        return spock.get(userSpock);
+    }
 }
 
--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -10,14 +10,15 @@ public class RpsRunner {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         boolean end = false;
-       // int gameComputer;
+        // int gameComputer;
         List<Integer> userResult = new ArrayList<>();
         List<Integer> compResult = new ArrayList<>();
-        ComputerRunner computerRunner=new ComputerRunner();
-        int gameComputer=0;
+        ComputerRunner computerRunner = new ComputerRunner();
+       // int gameComputer = 0;
 
 
         while (!end) {
+            int gameComputer = 0;
             userResult.clear();
             compResult.clear();
             System.out.println("Type your name");
@@ -30,6 +31,8 @@ public class RpsRunner {
             System.out.println("1-rocket");
             System.out.println("2-paper");
             System.out.println("3-scissors");
+            System.out.println("4-blizzard");
+            System.out.println("5-spock");
             System.out.println("x-end");
             System.out.println("n-once Again");
 
@@ -39,29 +42,39 @@ public class RpsRunner {
                 String gameNumber = scanner.nextLine();
                 int choseNumber = Integer.parseInt(gameNumber);
                 //gameComputer = random.nextInt(3) + 1;
-                if (choseNumber == 1 || choseNumber == 2 || choseNumber == 3) {
+                if (choseNumber == 1 || choseNumber == 2 || choseNumber == 3 || choseNumber == 4 || choseNumber == 5) {
                     if (choseNumber == 1) {
                         System.out.println("Your Choice: rocket");
-                        gameComputer= computerRunner.userRocket();
+                        gameComputer = computerRunner.userRocket();
                     } else if (choseNumber == 2) {
                         System.out.println("Your Choice: paper");
-                        gameComputer=computerRunner.userScissors();
-                    } else {
+                        gameComputer = computerRunner.userPaper();
+                    } else if (choseNumber == 3) {
                         System.out.println("Your choice: scissors");
-                        gameComputer=computerRunner.userPaper();
+                        gameComputer = computerRunner.userScissors();
+                    } else if (choseNumber == 4) {
+                        System.out.println("Your choice: blizzard");
+                        gameComputer = computerRunner.userBlizzard();
+                    } else if (choseNumber == 5) {
+                        System.out.println("Your choice: spock");
+                        gameComputer = computerRunner.userSpock();
                     }
-
                     if (gameComputer == 1) {
                         System.out.println("Computer: rocket");
-
                         if (choseNumber == 3) {
                             System.out.println("Computer Wins");
                             compResult.add(1);
                         } else if (choseNumber == 2) {
                             System.out.println(name + "Wins");
                             userResult.add(1);
-                        } else {
+                        } else if (choseNumber == 1) {
                             System.out.println("Semi");
+                        } else if (choseNumber == 4) {
+                            System.out.println("Computer Wins");
+                            compResult.add(1);
+                        } else {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
                         }
                     } else if (gameComputer == 2) {
                         System.out.println("Computer: paper");
@@ -71,12 +84,21 @@ public class RpsRunner {
                             compResult.add(1);
                         } else if (choseNumber == 2) {
                             System.out.println("Semi");
-                        } else {
+                        } else if (choseNumber == 1) {
                             System.out.println(name + "Wins");
                             userResult.add(1);
+                        } else if (choseNumber == 3) {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
+                        } else if (choseNumber == 4) {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
+                        } else {
+                            System.out.println("Computer Wins");
+                            compResult.add(1);
                         }
 
-                    } else {
+                    } else if (gameComputer == 3) {
                         System.out.println("Computer: scissors");
 
                         if (choseNumber == 2) {
@@ -85,35 +107,74 @@ public class RpsRunner {
                         } else if (choseNumber == 1) {
                             System.out.println(name + "Wins");
                             userResult.add(1);
+                        } else if (choseNumber == 3) {
+                            System.out.println("Semi");
+                        } else if (choseNumber == 4) {
+                            System.out.println("Computer Wins");
+                            compResult.add(1);
+                        } else {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
+                        }
+                    } else if (gameComputer == 4) {
+                        System.out.println("Computer: blizzard");
+                        if (choseNumber == 2) {
+                            System.out.println("Computer Wins");
+                            compResult.add(1);
+                        } else if (choseNumber == 1) {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
+                        } else if (choseNumber == 3) {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
+                        } else if (choseNumber == 4) {
+                            System.out.println("Semi");
+                        } else {
+                            System.out.println("Computer Wins");
+                            compResult.add(1);
+                        }
+                    } else if (gameComputer == 5) {
+                        System.out.println("Computer: Spock");
+                        if (choseNumber == 2) {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
+                        } else if (choseNumber == 1) {
+                            System.out.println("Computer Wins");
+                            compResult.add(1);
+                        } else if (choseNumber == 3) {
+                            System.out.println("Computer Wins");
+                            compResult.add(1);
+                        } else if (choseNumber == 4) {
+                            System.out.println(name + "Wins");
+                            userResult.add(1);
                         } else {
                             System.out.println("Semi");
                         }
+                    } else {
+                        System.out.println("Wrong number, try value from the range");
+                        break;
                     }
-                } else {
-                    System.out.println("Wrong number, try value from the range");
-                    break;
+                    System.out.println("Round: " + (i + 1) + ":" + "Computer result: " + compResult.size() + ", " + name + " result: " + userResult.size());
                 }
-                System.out.println("Round: " + (i + 1) + ":" + "Computer result: " + compResult.size() + ", " + name + " result: " + userResult.size());
+                System.out.println("Final result: " + "Computer result: " + compResult.size() + ", " + name + " result: " + userResult.size());
+                System.out.println("Type x- to end, n-once again");
+
             }
-            System.out.println("Final result: " + "Computer result: " + compResult.size() + ", " + name + " result: " + userResult.size());
-            System.out.println("Type x- to end, n-once again");
-
-
-            Scanner scan = new Scanner(System.in);
-            String game = scan.nextLine();
-            if (game.equals("x")) {
-                System.out.println("Are you sure: type x ");
-                String gameClose = scan.nextLine();
-                if (gameClose.equals("x")) {
-                    end = true;
-                } else
-                    System.out.println(("Wrong choice"));
-            } else if (game.equals("n")) {
-                end = false;
-            } else {
-                System.out.println("Wrong choice");
+                Scanner scan = new Scanner(System.in);
+                String game = scan.nextLine();
+                if (game.equals("x")) {
+                    System.out.println("Are you sure: type x ");
+                    String gameClose = scan.nextLine();
+                    if (gameClose.equals("x")) {
+                        end = true;
+                    } else
+                        System.out.println(("Wrong choice"));
+                } else if (game.equals("n")) {
+                    end = false;
+                } else {
+                    System.out.println("Wrong choice");
+                }
             }
         }
     }
-}
 
