df380c2058e7cd0bdc356654f9e8f9aecdffc7f5
Maciek2k3 <maciej.palczewski88@gmail.com>
2020-11-28 22:03:49 +0100

    13.4 SOLID, Food2Door
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/FoodToDoor/MailService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/FoodToDoor/MailService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class MailService implements InfoService {
     @Override
     public void information(Supplier supplier, Customer customer, Map<Product, Integer> orderList, int price) {
+        System.out.println("Hello"+customer.getName()+"ordered from: "+supplier+"yourOrder"+orderList.entrySet());
 
     }
 }
