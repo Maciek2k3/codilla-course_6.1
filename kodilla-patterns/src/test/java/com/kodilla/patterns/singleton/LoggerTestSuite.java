290846c2bdfda249193f5e2a40dc9d791f4f6deb
Maciek2k3 <maciej.palczewski88@gmail.com>
2020-12-09 13:41:57 +0100

    15.1 Singleton
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoggerTestSuite {
+    private static Logger logger;
+
+    @BeforeAll
+    public static void setLogger() {
+        logger = Logger.INSTANCE;
+        logger.log("Log1");
+    }
+
+    @Test
+    void testGetLog() {
+        //Given
+        // When
+        String loggin = logger.getLastLog();
+        //Then
+        assertEquals("Log1",loggin);
+    }
+
+}
