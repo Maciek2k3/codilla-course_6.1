c40c2368100021c91c9f9df32a68c869e0974bf5
Maciek2k3 <maciej.palczewski88@gmail.com>
2021-02-21 21:10:21 +0100

    24.5 Adapter
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,18 +1,18 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
-import com.kodilla.patterns2.adapter.bookclasifier.librarya.Clasyfier;
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class MedianAdapter extends MedianAdaptee implements Clasyfier {
+public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
     public int publicationYearMedian(Set<Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> bookMap = new HashMap<>();
-        for (com.kodilla.patterns2.adapter.bookclasifier.librarya.Book book : bookSet) {
+        for (Book book : bookSet) {
             com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book book1 =
                     new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book(book.getAuthor(), book.getSignature(), book.getPublicationYear());
             bookMap.put(new BookSignature(book.getSignature()), book1);
--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/librarya/Classifier.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/librarya/Classifier.java
@@ -2,7 +2,7 @@ package com.kodilla.patterns2.adapter.bookclasifier.librarya;
 
 import java.util.Set;
 
-public interface Clasyfier {
+public interface Classifier {
     int publicationYearMedian(Set<Book> bookSet);
 }
 
