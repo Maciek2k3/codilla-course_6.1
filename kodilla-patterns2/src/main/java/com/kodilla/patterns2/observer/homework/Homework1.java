1f62b179b7fb788e1cf628bc46f0ee7b0435a5e0
Maciek2k3 <maciej.palczewski88@gmail.com>
2021-02-21 21:07:08 +0100

    24.5 Adapter
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Homework1.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Homework1.java
@@ -1,7 +1,8 @@
 package com.kodilla.patterns2.observer.homework;
 
-public class Homework1 extends Student {
-    public Homework1() {
-        super("Taks1");
+public class Homework1 {
+    public void Homework1() {
+        System.out.println("task1");
+
     }
 }
--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Homework2.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.patterns2.observer.homework;
-
-public class Homework2 extends Student{
-    public Homework2() {
-        super("Task2");
-    }
-}
