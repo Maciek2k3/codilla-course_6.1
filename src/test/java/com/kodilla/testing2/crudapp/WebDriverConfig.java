7d1725e0c55b810ecd7298e8b797a1f5488c3f6c
Maciek2k3 <maciej.palczewski88@gmail.com>
2021-04-21 13:13:36 +0200

    31.1 Selenium
    Signed-off-by: Maciek2k3 <maciej.palczewski88@gmail.com>

---
+++ src/test/java/com/kodilla/testing2/crudapp/WebDriverConfig.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing2.crudapp;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverConfig {
+        public final static String CHROME = "CHROME_DRIVER";
+        public static WebDriver getDriver(final String driver) {
+            System.setProperty("webdriver.chrome.driver", "/GooglePath/chromedriver");
+            if (driver.equals(CHROME)) {
+                return new ChromeDriver();
+            } else {
+                return null;
+            }
+        }
+    }
